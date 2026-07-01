f53b017a5cbb047bccdab9c96b1615acf50438e8
kanak <kanakkhaitan15@gmail.com>
2026-07-01 15:33:44 +0530

    ho gya

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -14,6 +14,8 @@ API.interceptors.request.use(
   (req) => {
     const token = localStorage.getItem("token");
 
+    console.log("Token =", localStorage.getItem("token"));
+
     if (token) {
       req.headers.Authorization = `Bearer ${token}`;
     }
